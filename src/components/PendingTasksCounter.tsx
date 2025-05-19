fe8c88de5e4bcf7088fbdd6cf9ff55a4c7f03f50
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-05-19 13:45:03 +0300

    improve app

--- src/components/PendingTasksCounter.tsx
+++ src/components/PendingTasksCounter.tsx
@@ -1,3 +1,17 @@
-export function PendingTasksCounter() {
-  return <div>Pending Tasks Counter</div>;
+import { getStore } from "../stores/StoreManager";
+import * as React from "react";
+
+// Minimal custom hook to subscribe to nanostores atom in React
+function useStore(store) {
+  const [value, setValue] = React.useState(store.get());
+  React.useEffect(() => {
+    return store.subscribe(setValue);
+  }, [store]);
+  return value;
+}
+
+export function PendingTasksCounter(props: { storeId: string }) {
+  const theStore = getStore(props.storeId);
+  const totalCount = theStore ? useStore(theStore.totalCount) : 0;
+  return <div>Pending Tasks: {totalCount}</div>;
 }
