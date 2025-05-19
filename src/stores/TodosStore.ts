1bc2ad622c19255f1d39bb393072839270a03b46
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-05-19 13:55:41 +0300

    improve app

--- src/stores/TodosStore.ts
+++ src/stores/TodosStore.ts
@@ -3,32 +3,27 @@ import { atom, computed } from "nanostores";
 export type Todo = {
   id: string;
   text: string;
-  completed: boolean;
 };
 
 export function createTodosStore(id: string) {
-  // Atom to hold the list of todos
   const todos = atom<Todo[]>([]);
-
-  // Actions
-  function addTodo(text: string) {
-    todos.set([
-      ...todos.get(),
-      { id: crypto.randomUUID(), text, completed: false },
-    ]);
-  }
-
-  function removeTodo(todoId: string) {
-    todos.set(todos.get().filter((todo) => todo.id !== todoId));
-  }
-
-  // Getters
   const totalCount = computed(todos, (list) => list.length);
 
+  const addTodo = (text: string) => {
+    todos.set([...todos.get(), { id: crypto.randomUUID(), text }]);
+  };
+
+  const removeTodo = (todoId: string) => {
+    todos.set(todos.get().filter((todo) => todo.id !== todoId));
+  };
+
   return {
+    // Getters
     todos,
+    totalCount,
+
+    // Actions
     addTodo,
     removeTodo,
-    totalCount,
   };
 }
